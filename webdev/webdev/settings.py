13c22c5917ba0b71202b557790d03ed00e2e4295
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-08-14 16:38:00 -0300

    static_root fix

--- webdev/webdev/settings.py
+++ webdev/webdev/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = "/home/myusername/myproject/static"
+STATIC_ROOT = "/home/arthur133/mycontacts/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
